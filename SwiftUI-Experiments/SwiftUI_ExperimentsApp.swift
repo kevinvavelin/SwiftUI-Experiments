660856f9d8b854a308b8a3b7b7d1af3fde34a14f
Kevin Vavelin <kvavelin@geek-inc.fr>
2020-08-20 14:57:36 +0200

    Initial Commit

---
+++ SwiftUI-Experiments/SwiftUI_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExperimentsApp.swift
+//  SwiftUI-Experiments
+//
+//  Created by Kevin Vavelin on 20/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
